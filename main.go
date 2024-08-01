5140f938085be252b72f12bc2e1c02b783f4cb7d
Cauê Zaratin <caue.lasagno.zaratin@gmail.com>
2024-08-01 17:25:18 -0300

    feat: created a new project already with a few handlers and routes

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/cauelz/golang-event-booking-rest-api/models"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	server := gin.Default()
+
+	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
+
+	server.Run(":8080")
+}
+
+func getEvents(c *gin.Context) {
+
+	events := models.GetAllEvents()
+
+	c.JSON(http.StatusOK, events)
+}
+
+func createEvent(c *gin.Context) {
+	var event models.Event
+
+	err := c.ShouldBindBodyWithJSON(&event)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Could not bind JSON"})
+		return
+	}
+
+	event.ID = 1
+	event.UserId = 1
+
+	event.Save()
+
+	c.JSON(http.StatusCreated, gin.H{"status": "Event created successfully", "event": event})
+}
---
+++ models/event.go
@@ -0,0 +1,22 @@
+package models
+
+import "time"
+
+type Event struct {
+	ID		  int    `json:"id"`
+	Name 	string `json:"name" binding:"required"`
+	Description string `json:"description" binding:"required"`
+	Location string `json:"location" binding:"required"`
+	DateTime time.Time `json:"date_time" binding:"required"`
+	UserId int `json:"user_id"`
+}
+
+var events = []Event{}
+
+func (e Event) Save() {
+	events = append(events, e)
+}
+
+func GetAllEvents() []Event {
+	return events
+}
